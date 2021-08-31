e6f1c0fbb227e6ef5bd2f8ca60d4183e64686d83
Matt Griffith <mattgriffith0@gmail.com>
2021-08-31 16:53:38 -0700

    add edit password and add forgot/reset password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,5 @@ class ApplicationController < ActionController::Base
 
   def require_user_logged_in!
     redirect_to sign_in_path, alert: "You must be signed in to do that!" if Current.user.nil?
-
   end
 end
