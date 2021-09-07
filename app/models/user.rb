00bf1b42bd41487bc034894a4c102b6a74960df4
Matt Griffith <mattgriffith0@gmail.com>
2021-09-07 14:31:47 -0700

    add tweet model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@
 
 class User < ApplicationRecord
   has_many :twitter_accounts
+  has_many :tweets
   has_secure_password
 
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
